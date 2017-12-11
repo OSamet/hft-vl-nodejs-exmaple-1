aed64fd4158d5e0865800d99b785c8f356fc77d0
Mathias Maciossek <mathias.maciossek@gmail.com>
2017-12-11 09:42:29 +0100

    initial commit

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+  const pageTitle = 'This is a title';
+  const anArrayOfData = ['This', 'is', 'something', 'to', 'loop'];
+
+
+  res.render('pages/index', {
+    title: pageTitle,
+    data: anArrayOfData
+  });
 });
 
 app.get('/about', function(req, res) {
-    res.render('pages/about');
+  res.render('pages/about');
 });
 
 app.listen(port, function() {
